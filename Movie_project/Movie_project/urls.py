e1593a724a98d39434c966c72420c0099e2f79a7
KVadim-K <kvadimk@yandex.ru>
2024-08-13 21:48:02 +0300

    add little

--- Movie_project/Movie_project/urls.py
+++ Movie_project/Movie_project/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView  # Добавлено перенаправление RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('films.urls')),
-    path('admin_pan/', include('admin_pan.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('films/', include('films.urls')),  # Добавлено перенаправление films/
+                  path('admin_pan/', include('admin_pan.urls')),
+                  path('', RedirectView.as_view(url='/admin_pan/', permanent=True)),  # Добавлено перенаправление
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
